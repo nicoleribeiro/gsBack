9940ac91a46223fcac296994b1a180fbc4a06ca9
nicoleribeiro <nicole.ribeiro1612@gmail.com>
2022-06-09 22:51:47 -0300

    ajustes finais

--- app/models/Mensagem.php
+++ app/models/Mensagem.php
@@ -54,42 +54,4 @@ class Mensagem
             throw new \Exception("Falha ao inserir mensagem!");
         }
     }
-
-    public static function update($data, $id)
-    {
-        $connPdo = new \PDO(DBDRIVE . ': host=' . DBHOST . '; dbname=' . DBNAME, DBUSER, DBPASS);
-
-        $sql = 'UPDATE ' . self::$table . 'SET nome = :nome, email = :email, mensagem = :mensagem WHERE id = :id';
-        $stmt = $connPdo->prepare($sql);
-        $stmt->bindValue(':id', $id);
-        $stmt->bindValue(':nome', $data['nome']);
-        $stmt->bindValue(':email', $data['email']);
-        $stmt->bindValue(':mensagem', $data['mensagem']);
-        $stmt->execute();
-
-        if ($stmt->rowCount() > 0) {
-            return 'Mensagem alterada com sucesso!';
-        } else {
-            throw new \Exception("Falha ao alterar mensagem!");
-        }
-    }
-
-    public static function delete($data, $id)
-    {
-        $connPdo = new \PDO(DBDRIVE . ': host=' . DBHOST . '; dbname=' . DBNAME, DBUSER, DBPASS);
-
-        $sql = 'DELETE FROM ' . self::$table . 'WHERE id = :id';
-        $stmt = $connPdo->prepare($sql);
-        $stmt->bindValue(':id', $id);
-        $stmt->bindValue(':nome', $data['nome']);
-        $stmt->bindValue(':email', $data['email']);
-        $stmt->bindValue(':mensagem', $data['mensagem']);
-        $stmt->execute();
-
-        if ($stmt->rowCount() > 0) {
-            return 'Mensagem alterada com sucesso!';
-        } else {
-            throw new \Exception("Falha ao alterar mensagem!");
-        }
-    }
 }
